0e026edd063ae3e271b952c0f086c71be0eeb1d2
Hasse <imberg.hasse@pm.me>
2024-01-26 10:44:59 +0200

    refactored HTML message in other file module

--- backend/src/utils/sendVerification.ts
+++ backend/src/utils/sendVerification.ts
@@ -1,29 +1,21 @@
 import { transporter } from "../config/nodeMailer";
+import getVerificationMessage from "./verificationMessage";
 
 export default function sendVerification(
   email: string,
   verificationCode: string,
   expirationDate: Date
 ) {
-  const htmlMessage = `
-    <html>
-      <body>
-        <p>Hyvä käyttäjä,</p>
-        <p>Kiitos rekisteröitymisestäsi. Saattaaksesi rekisteröitymisesi päätökseen, ole hyvä ja vahvista sähköpostiosoitteesi syöttämällä seuraava vahvistuskoodi:</p>
-        <p><b>${verificationCode}</b></p>
-        <p>Huomioithan, että tämä koodi vanhenee <b>${expirationDate.toDateString()}</b>.</p>
-        <p>Jos et pyytänyt tätä vahvistusta, voit jättää tämän sähköpostin huomiotta.</p>
-        <p>Parhain terveisin,<br>lapsen-arki.fi</p>
-      </body>
-    </html>
-  `;
-
+  const verificationMessge = getVerificationMessage(
+    verificationCode,
+    expirationDate
+  );
   let mailOptions = {
     from: `Testing" <${process.env.MAIL_USER}>`, // Sender address
     to: email, // List of recipients
     subject: "Sähköpostin vahvistus", // Subject line
     text: "Vahvista sähköpostiosoitteesi", // Plain text body
-    html: htmlMessage, // HTML body
+    html: verificationMessge, // HTML body
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
