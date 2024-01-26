c3e8de1ccc7c60c01e7be774cbe6d9c6f0d838b5
jholopai <jholopai@users.noreply.github.com>
2024-01-26 08:22:40 +0200

    Add initial home layout

--- frontend/src/views/Home.tsx
+++ frontend/src/views/Home.tsx
@@ -1,36 +1,60 @@
-import { useContext } from "react";
-import { TokenContext } from "../contexts/tokenContext";
+import React from 'react'
+import { useState } from 'react'
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
-// PYYHI TESTI ROSKAT POIS JOS TULET TEKEMÄÄN
 
 export default function Home() {
-  const { isLoggedIn, idToken, signOutMethod } = useContext(TokenContext);
+	const [target, setTarget] = useState("")
+	const [insertOn, setInsertOn] = useState(false)
 
-  function signOuttt() {
-    console.log("logging out");
-
-    signOutMethod();
-  }
-  function printToken() {
-    console.log(idToken);
-    console.log(isLoggedIn);
-  }
-
+	const handleChange = ( setState: React.Dispatch<React.SetStateAction<string>>, value: string ) => {
+		setState(value);
+		setInsertOn(false) //väliaikainen ratkaisu buildeja varten
+	};
+	
+	const handleSelectChange = (stateSetter: React.Dispatch<React.SetStateAction<string>>) => ( e: SelectChangeEvent<string | number> ) => {
+		handleChange(stateSetter, e.target.value as string);
+	};
   return (
-    <>
-      <h1>Etusivu</h1>
-      <button onClick={signOuttt}>signOut</button>
-      <button onClick={printToken}>printToken</button>
-    </>
-  );
+		<div className="preview-page">
+			<div className="preview-top-panel">
+				<div className="preview-image">
+
+				</div>
+				<div className="preview-top-right">
+					<div className="preview-description">
+						<h1>Lapsen arkirytmi</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+					</div>
+					<div className="preview-top-right-bottom">
+						<div className="preview-grid-title">
+							{insertOn?
+							<h2>Luo ateria</h2>
+							:null
+							}
+						</div>
+						<div className="preview-target">
+							<FormControl fullWidth>
+								<InputLabel id="preview-target-label">Lapsi</InputLabel>
+								<Select
+									labelId="preview-target-label"
+									id="preview-target-select"
+									value={target}
+									label="Lapsi"
+									onChange={handleSelectChange(setTarget)}
+									>
+									<MenuItem value={"Ulpukka"}>Ulpukka</MenuItem>
+									<MenuItem value={"Kullervo"}>Kullervo</MenuItem>
+								</Select>
+							</FormControl>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+  	);
 }
 
-/* 
-export default function Home() {
-  return (
-    <>
-      <h1>Etusivu</h1>
-    </>
-  );
-}
-*/
