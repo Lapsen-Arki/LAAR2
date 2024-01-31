25c527be26a1373d94495a1633a9f8097f02d612
Hasse331 <imberg.hasse@pm.me>
2024-01-31 11:01:42 +0200

    Found 100x simplier solution

--- backend/src/api/controllers/register/register.ts
+++ backend/src/api/controllers/register/register.ts
@@ -29,6 +29,30 @@ const registerUser = async (req: Request, res: Response) => {
       );
     }
 
+    let customer = { id: "" };
+    let subscription = { id: "" };
+    try {
+      const stripe = stripeConf();
+
+      const customer = await stripe.customers.create({
+        email: email,
+        source: tokenId,
+      });
+
+      // Starting new subscription and 14 day trial:
+      const subscription = await stripe.subscriptions.create({
+        customer: customer.id,
+        items: [{ plan: "price_1ObLeAK45umi2LZd5XwwYvam" }], // THIS IS TEST PLAN -> CHANGE FOR PRODUCTION
+        trial_period_days: 14,
+        cancel_at_period_end: true,
+      });
+
+      console.log("Subscription created:", subscription);
+      return customer.id;
+    } catch (error) {
+      console.error("Error creating subscription: ", error);
+    }
+
     // Create a new user using Firebase Authentication
     const userRecord = await admin.auth().createUser({
       email: email,
@@ -48,32 +72,15 @@ const registerUser = async (req: Request, res: Response) => {
       name: isValidName,
       email: email,
       registrationDate: registrationDate,
-      stripeTokenId: tokenId,
+      stripeCustomerId: customer.id,
+      stripeCardTokenId: tokenId,
+      stripeSubscriptionId: subscription.id,
       emailVerified: false,
       verificationCode: verificationCode,
       codeExpires: expirationDate,
     });
 
     sendVerification(email, verificationCode, expirationDate);
-
-    try {
-      const stripe = stripeConf();
-
-      const customer = await stripe.customers.create({
-        email: email,
-        source: tokenId,
-      });
-
-      // Starting new subscription and 14 day trial:
-      const subscription = await stripe.subscriptions.create({
-        customer: customer.id,
-        items: [{ plan: "price_1ObLeAK45umi2LZd5XwwYvam" }], // THIS IS TEST PLAN -> CHANGE FOR PRODUCTION
-        trial_period_days: 14,
-      });
-      console.log("Subscription created:", subscription);
-    } catch (error) {
-      console.error("Error creating subscription: ", error);
-    }
 
     res
       .status(201)
