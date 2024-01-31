155e36e82bd94a61a10ceae6ea7eadf6671f4329
Esa Leino <esaleino93@gmail.com>
2024-01-31 12:36:15 +0200

    Initial commit

--- backend/tests/testTypes.ts
+++
@@ -1,7 +0,0 @@
-export interface ChildProfile {
-  id: string;
-  accessRights: boolean;
-  avatar: string;
-  birthdate: string;
-  childName: string;
-}
--- workflow-test.sh
+++
@@ -1,6 +0,0 @@
-FIREBASE_KEY_JSON=$(cat .env | grep FIREBASE_KEY_JSON | cut -d '=' -f2)
-
-# Run local-initialize and wait for completion
-docker pull ghcr.io/catthehacker/ubuntu:full-latest
-sudo act -P self-hosted=ghcr.io/catthehacker/ubuntu:full-latest --env FIREBASE_KEY_JSON="$FIREBASE_KEY_JSON"
-
