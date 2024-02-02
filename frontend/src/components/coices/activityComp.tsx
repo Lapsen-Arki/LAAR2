c4cfcc1e1b7951d698ea6c0c4f92aa270a2a3d16
Hasse <imberg.hasse@pm.me>
2024-02-02 15:40:41 +0200

    päivitetty choices.tsx

---
+++ frontend/src/components/coices/activityComp.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+
+export default function ActivityComp() {
+  return (
+    <>
+      <Typography variant="h4">Aktiviteetteja:</Typography>
+      <Typography>
+        Lapsen ikään sopivia aktiviteetteja: (map methodilla)
+      </Typography>
+    </>
+  );
+}
