c3b933a782f45436468cc5d678fea68b910c2c6d
Hasse <imberg.hasse@pm.me>
2024-02-07 10:14:45 +0200

    updated getChildProfiles

--- frontend/src/api/childProfile/getChildProfiles.ts
+++ frontend/src/api/childProfile/getChildProfiles.ts
@@ -12,39 +12,51 @@ interface ChildProfile {
 }
 
 const getChildProfiles = async (idToken: string | null) => {
-    try {
-        console.log("Haetaan profiileja...");
-        const config = {
-            headers: {
-                Authorization: `Bearer ${idToken}`
-            }
-        };
-        const response = await axios.get<ChildProfile[]>(`${API_BASE_URL}/profiles`, config);
-        console.log("Profiilit haettu onnistuneesti:", response.data);
-
-        // Tallenna profiilit Session Storageen, jos ne ovat muuttuneet
-        updateSessionStorage(response.data);
-
-        return response.data;
-    } catch (error) {
-        if (axios.isAxiosError(error) && error.response) {
-            console.error("Profiilien hakeminen epäonnistui:", error.response.data);
-            return { error: error.response.data };
-        }
-        console.error("Virhe profiileja haettaessa:", error);
-        throw error;
+  try {
+    console.log("Haetaan profiileja...");
+    const config = {
+      headers: {
+        Authorization: `Bearer ${idToken}`,
+      },
+    };
+    const storedProfilesJson = sessionStorage.getItem("childProfiles");
+    let response;
+    if (!storedProfilesJson) {
+      response = await axios.get<ChildProfile[]>(
+        `${API_BASE_URL}/profiles`,
+        config
+      );
+      console.log("Profiilit haettu onnistuneesti:", response.data);
     }
+
+    // Tallenna profiilit Session Storageen, jos ne ovat muuttuneet
+    if (response) {
+      updateSessionStorage(response.data);
+      return response.data;
+    } else {
+      console.log("profillit löytyi sessionStoragesta");
+    }
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      console.error("Profiilien hakeminen epäonnistui:", error.response.data);
+      return { error: error.response.data };
+    }
+    console.error("Virhe profiileja haettaessa:", error);
+    throw error;
+  }
 };
 
 const updateSessionStorage = (profiles: ChildProfile[]) => {
-    const storedProfilesJson = sessionStorage.getItem("childProfiles");
-    const storedProfiles = storedProfilesJson ? JSON.parse(storedProfilesJson) : [];
+  const storedProfilesJson = sessionStorage.getItem("childProfiles");
+  const storedProfiles = storedProfilesJson
+    ? JSON.parse(storedProfilesJson)
+    : [];
 
-    // Vertaa haettuja profiileja tallennettuihin
-    if (JSON.stringify(profiles) !== JSON.stringify(storedProfiles)) {
-        console.log("Profiilit ovat muuttuneet, päivitetään Session Storage");
-        sessionStorage.setItem("childProfiles", JSON.stringify(profiles));
-    }
+  // Vertaa haettuja profiileja tallennettuihin
+  if (JSON.stringify(profiles) !== JSON.stringify(storedProfiles)) {
+    console.log("Profiilit ovat muuttuneet, päivitetään Session Storage");
+    sessionStorage.setItem("childProfiles", JSON.stringify(profiles));
+  }
 };
 
 export { getChildProfiles };
--- frontend/src/views/index.tsx
+++ frontend/src/views/index.tsx
@@ -2,15 +2,22 @@ import { Container } from "@mui/material";
 
 import LandingComp from "../components/index/landingComp";
 import TimeBlockComp from "../components/index/timeBlockComp";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { TokenContext } from "../contexts/tokenContext";
 import NameDropDown from "../components/index/nameDropDown";
 
-export default function IndexPage() {
-  const { isLoggedIn } = useContext(TokenContext);
+import { getChildProfiles } from "../api/childProfile/getChildProfiles";
 
-  // 1. Have to fetch child age and name at some point -> using useContext
-  // may be easiest way. And savin it to sessionStorage
+export default function IndexPage() {
+  const { isLoggedIn, idToken } = useContext(TokenContext);
+
+  // 1. Have to fetch child age and name at some point -> saving to sessionStorage
+  useEffect(() => {
+    if (isLoggedIn) {
+      // Fetching childProfiles
+      getChildProfiles(idToken);
+    }
+  }, [idToken, isLoggedIn]);
 
   return (
     <>
