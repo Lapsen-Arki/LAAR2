4db094f835ea2a2b6292e2c8e5cc231333ac838c
Elias <elias.saukkonen@gmail.com>
2024-02-14 11:41:49 +0200

    Poistaminen toimii arraysta

--- backend/src/api/controllers/carersProfile/deleteCarerProfile.ts
+++ backend/src/api/controllers/carersProfile/deleteCarerProfile.ts
@@ -1,64 +1,72 @@
+/*
+  Jätetty console.logit kommentteihin jos haluaa tarkastella, miten koodi toimii.
+*/
+
+
 import { Request, Response } from "express";
 import admin from "../../../config/firebseConfig";
 import { getUserIdFromToken } from "../../../utils/getUserIdFromTokenUtil";
 
 interface CarerProfile {
   receiverUid: string;
-  email: string;
-  name: string;
   senderUid: string[];
 }
 
 const deleteCarerProfile = async (req: Request, res: Response): Promise<void> => {
   const carerId: string | undefined = req.params.carerId;
-  console.log("Saatu carerId:", carerId);
+  //console.log("Saatu carerId:", carerId); // console log
 
   try {
     const idToken: string | undefined = req.headers.authorization?.split("Bearer ")[1];
     if (!idToken) {
-      console.log("Token puuttuu");
+      //console.log("Token puuttuu"); // console log
       res.status(401).json({ error: "Token puuttuu" });
       return;
     }
 
     const removerId: string | null = await getUserIdFromToken(idToken);
+    //console.log("removerId:", removerId); // console log
     if (!removerId) {
-      console.log("Virheellinen token");
+      //console.log("Virheellinen token"); // console log
       res.status(403).json({ error: "Virheellinen token" });
       return;
     }
 
-    if (!carerId) {
-      console.log("Tarvittavat tiedot puuttuvat: carerId");
-      res.status(400).json({ error: "Tarvittavat tiedot puuttuvat: carerId" });
+    const db = admin.firestore();
+    const querySnapshot = await db.collection('childCarers').where('receiverUid', '==', carerId).get();
+
+    if (querySnapshot.empty) {
+      //console.log("Hoitajaprofiilia ei löytynyt."); // console log
+      res.status(404).json({ error: "Hoitajaprofiilia ei löytynyt." });
       return;
     }
 
-    const db = admin.firestore();
+    // Käy läpi löydetyt dokumentit ja poista removerId senderUid-kentästä
+    querySnapshot.forEach(async (doc: FirebaseFirestore.DocumentSnapshot) => {
+      try {
+        //console.log("Dokumentin data:", doc.data()); // console log
+        const carerData = doc.data() as CarerProfile;
+        //console.log("CarerData:", carerData); // console log
     
-    // Käytä collection-metodia kaikkien childCarers-kokoelman alikokoelmien hakuun
-    const querySnapshot = await db.collection('childCarers').where('receiverUid', '==', carerId).get();
-
-    res.status(200).json({ message: "Hoitajan profiili poistettu onnistuneesti" });
-  } catch (error: any) {
-    console.error("Hoitajan profiilin poisto epäonnistui", error);
+        if (carerData.senderUid.includes(removerId)) {
+          const updatedSenderUids = carerData.senderUid.filter((uid: string) => uid !== removerId);
+          await doc.ref.update({
+            senderUid: updatedSenderUids
+          });
+          //console.log("Hoitajan profiili päivitetty:", updatedSenderUids); // console log
+        } else {
+          //console.log("Poistaminen ei tarpeen: removerId ei löytynyt senderUidista."); // console log
+        }
+      } catch (error) {
+        console.error("Dokumentin päivitys epäonnistui", error);
+      }
+    });
+    
+    res.status(200).json({ message: "Hoitajan profiili päivitetty onnistuneesti." });
+  } catch (error) {
+    console.error("Hoitajan profiilin päivitys epäonnistui", error);
     res.status(500).json({ error: "Jotain meni pieleen" });
   }
 };
 
 export { deleteCarerProfile };
-
-/*
-// Saving data to firebase
-    const db = admin.firestore();
-    const recommCollection = db.collection("recommendations");
-
-    let query = recommCollection
-      .where("category", "==", newData.category)
-      .where("type", "==", newData.type)
-      .where("title", "==", newData.title);
-
-    const querySnapshot = await query.get();
-
-    if (!querySnapshot.empty) {
-*/
