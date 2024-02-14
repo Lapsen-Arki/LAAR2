b94110f6db316a23de5aeabba877daecdb8c19cd
Elias <elias.saukkonen@gmail.com>
2024-02-14 13:19:20 +0200

    Build error korjattu, tyypit uudessa paikassa

--- frontend/src/api/childProfile/deleteChildProfile.ts
+++ frontend/src/api/childProfile/deleteChildProfile.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { ChildProfile } from "../../../src/types/types";
+import { ChildProfile } from "../../types/typesFrontend";
 
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api";
