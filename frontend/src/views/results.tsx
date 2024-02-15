13249345d5f24fad90e2865275b9edd584bbfbe9
Hasse <imberg.hasse@pm.me>
2024-02-15 14:06:03 +0200

    page title pos

--- frontend/src/views/results.tsx
+++ frontend/src/views/results.tsx
@@ -30,7 +30,9 @@ export default function Results() {
   return (
     <div>
       <ReturnBtn />
-      <Typography variant="h3">Tulokset</Typography>
+      <Typography variant="h3" sx={{ textAlign: "center" }}>
+        Tulokset
+      </Typography>
       <Grid
         container
         sx={{ mt: 5, mb: 15, textAlign: "center", justifyContent: "center" }}
