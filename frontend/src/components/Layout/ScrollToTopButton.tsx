d16b4326d31fc6a0b76fc89407b900c6ef84510b
Johanna Kinnari <johanna.e.kinnari@gmail.com>
2024-02-28 14:37:08 +0200

    Ready to review

--- frontend/src/components/Layout/ScrollToTopButton.tsx
+++ frontend/src/components/Layout/ScrollToTopButton.tsx
@@ -1,59 +1,40 @@
-import React,{ useState, useEffect } from 'react'
-import {
-    Button
-  } from "@mui/material";
+import { useState, useEffect } from 'react'
+import { Button } from "@mui/material"
 
-const ScrollToTopButton: React.FC = () => {
-  const [visible, setVisible] = useState(false)
+const ScrollToTopButton = () => {
+  const [isVisible, setVisible] = useState(false)
 
-  const hideWhenVisible = { display: visible ? 'none' : '' }
-  const showWhenVisible = { display: visible ? '' : 'none' }
-
-  useEffect(() => {
+useEffect(() => { 
     const handleScroll = () => {
-      const screenHeight = window.innerHeight
-      const documentHeight = document.documentElement.scrollHeight
-      const scrollPosition = window.scrollY
-      const isMobile = /Mobi|Android/i.test(navigator.userAgent)
-      
-      if (isMobile && documentHeight >= screenHeight * 1) {
-        if (scrollPosition >= documentHeight - screenHeight) {
-          setVisible(true)
-        } else {
-          setVisible(false)
+        const screenHeight = window.innerHeight
+        const documentHeight = document.documentElement.scrollHeight
+        const isMobile = /Mobi|Android/i.test(navigator.userAgent)
+        if (isMobile && documentHeight >= screenHeight * 4) {
+            const scrollPosition = window.scrollY
+            setVisible(scrollPosition > window.innerHeight / 2)
         }
-      } else {
-        setVisible(false)
-      }
     }
-
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('scroll', handleScroll)
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+        window.removeEventListener('scroll', handleScroll)
     }
-  }, [])
+}, [])
 
   return (
     <div>
-      <div style={hideWhenVisible}>
-      </div>
-      <div style={{ ...showWhenVisible, position: 'relative', width: '100%', height: '100vh' }}>
-        <Button
-        variant="contained"
-        style={{  
-          position: 'absolute', 
-          bottom: '20px', 
-          right: '20px',
-          zIndex: 99,
-          }} 
-        onClick={() => window.scrollTo({ 
-          top: 0, 
-          behavior: "smooth" 
-          })}
-          >
+      <div style={{ position: 'fixed', bottom: '60px', right: '20px', zIndex: 99 }}>
+        {isVisible && (
+          <Button 
+          variant="contained" 
+          style = {{ backgroundColor: '#63c8cc'}} 
+          onClick={() => window.scrollTo({ 
+            top: 0, 
+            behavior: "smooth" 
+            })}>
             Sivun alkuun
           </Button>
+          )}
       </div>
     </div>
   )
