bf40c29b3fd90f0c867d9f5e049c7065407bad4e
Hasse <imberg.hasse@pm.me>
2024-03-08 12:11:25 +0200

    refactored recommComp.tsx

--- frontend/src/components/coices/recommComp.tsx
+++ frontend/src/components/coices/recommComp.tsx
@@ -8,13 +8,11 @@ import {
 } from "@mui/material";
 import { RecommendationsType } from "../../types/recommTypes";
 import React, { useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { TokenContext } from "../../contexts/tokenContext";
 import { getSubscriptionStatus } from "../../api/stripeSubscriptions";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
-import ShoppingListComp from "../shoppingListComp";
-import ReturnBtn from "../returnBtn";
+import RecommButtons from "./recommButtons";
 
 // Getting real child data somewhere:
 
@@ -40,9 +38,7 @@ export default function RecommComp({
   const [selectionList, setSelectionList] = useState<string[]>([]);
   const [subscribed, setSubscribed] = useState<boolean | null>(false);
   const [collapseOpen, setCollapseOpen] = useState<CollapseOpen>({});
-  const [shoppingList, setShoppingList] = useState<string[] | null>(null);
-  const { isLoggedIn, idToken } = useContext(TokenContext);
-  const navigate = useNavigate();
+  const { idToken } = useContext(TokenContext);
 
   useEffect(() => {
     const getSubStatus = async () => {
@@ -53,54 +49,13 @@ export default function RecommComp({
       }
     };
     getSubStatus();
-    const savedShoppingListJSON = localStorage.getItem("shoppingList");
-    if (savedShoppingListJSON) {
-      const savedShoppingList = JSON.parse(savedShoppingListJSON);
-      setShoppingList(savedShoppingList);
-    }
   }, [idToken]);
-
-  const handleShoppingList = () => {
-    // 1. Tallentaa tai päivittää ensin session storageen
-    const shoppingListJSON = localStorage.getItem("shoppingList");
-    if (!shoppingListJSON) {
-      localStorage.setItem("shoppingList", JSON.stringify(selectionList));
-      setShoppingList(selectionList);
-    } else {
-      const oldShoppingList = JSON.parse(shoppingListJSON);
-      const filteredList = selectionList.filter(
-        (item) => !oldShoppingList.includes(item)
-      );
-      const newList: string[] = [...oldShoppingList, ...filteredList];
-      localStorage.setItem("shoppingList", JSON.stringify(newList));
-      setShoppingList(newList);
-    }
-    // 2. 10-30 sekunnun timeoutin jälkeen tallentaa tietokantaan, eli kutsuu API function:
-    // Koska muuten: "eikun lisäänkin mansikat vielä ostoslistalle" tms
-  };
 
   const handleCollapse = (itemName: string) => {
     setCollapseOpen((prevCollapseOpen) => ({
       ...prevCollapseOpen,
       [itemName]: !prevCollapseOpen[itemName],
     }));
-  };
-
-  const handleClick = () => {
-    navigate("/results", {
-      state: { selectionList, isMealPage: mealType },
-    });
-    window.scrollTo(0, 0);
-  };
-
-  const registerNowClick = () => {
-    navigate("/register");
-    window.scrollTo(0, 0);
-  };
-
-  const subscribeNowClick = () => {
-    navigate("/subscription");
-    window.scrollTo(0, 0);
   };
 
   // Reset selectionList if selected child changes
@@ -258,72 +213,12 @@ export default function RecommComp({
         }
       })}
 
-      <div style={{ textAlign: "right" }}>
-        <div>
-          {/* Kokoa Ateria btn */}
-          {subscribed && isLoggedIn && selectedBox.length > 0 && (
-            <Button
-              onClick={handleClick}
-              sx={{ mt: 0.5, mb: 0.5, mr: 0.5 }}
-              variant="contained"
-            >
-              {mealType ? "Kokoa Ateria" : "Valitse aktiviteetti"}
-            </Button>
-          )}
-          {/* Ostoslistqa btn */}
-          {mealType && selectedBox.length > 0 && (
-            <Button
-              onClick={handleShoppingList}
-              sx={{ mt: 0.5, mb: 0.5, mr: 0.5 }}
-              variant="contained"
-            >
-              {shoppingList ? "Lisää ostoslistalle" : "Luo ostoslista"}
-            </Button>
-          )}
-        </div>
-
-        {/* Avaa kaikki ominaisuudet btn */}
-        {!isLoggedIn && selectedBox.length > 0 && (
-          <>
-            <Button
-              onClick={registerNowClick}
-              sx={{ mt: 0.5, mb: 0.5, mr: 0.5 }}
-              variant="contained"
-            >
-              Rekisteröidy nyt!
-            </Button>
-            <Typography>
-              Avaa kaikki ominaisuudet ja aloita 14 päivän ilmainen kokeilu!
-            </Typography>
-          </>
-        )}
-        {/* Jatka tilausta btn */}
-        {isLoggedIn && !subscribed && selectedBox.length > 0 && (
-          <>
-            <Button
-              onClick={subscribeNowClick}
-              sx={{ mt: 0.5, mb: 0.5, mr: 0.5 }}
-              variant="contained"
-            >
-              Jatka tilausta
-            </Button>
-            <Typography>Ja avaa kaikki ominaisuudet!</Typography>
-          </>
-        )}
-        <div style={{ marginTop: 50 }}>
-          <ReturnBtn message="Palaa etusivulle" />
-        </div>
-        {/* Shopping list: */}
-        {shoppingList && mealType && (
-          <div style={{ textAlign: "left" }}>
-            <ShoppingListComp shoppingList={shoppingList} />
-
-            {shoppingList.length > 15 && (
-              <ReturnBtn message="palaa etusivulle" />
-            )}
-          </div>
-        )}
-      </div>
+      <RecommButtons
+        subscribed={subscribed}
+        selectedBox={selectedBox}
+        mealType={mealType}
+        selectionList={selectionList}
+      />
     </div>
   );
 }
