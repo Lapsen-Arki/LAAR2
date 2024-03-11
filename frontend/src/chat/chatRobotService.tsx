b5d2b3892b5a17d96b9f232c515fae774a96b060
Elias <elias.saukkonen@gmail.com>
2024-03-11 12:34:57 +0200

    sää front fix

--- frontend/src/chat/chatRobotService.tsx
+++ frontend/src/chat/chatRobotService.tsx
@@ -1,5 +1,17 @@
+import axios from 'axios';
 import { robotAnswers } from './robotAnswers';
 import { Message } from '../types/typesFrontend';
+
+interface Forecast {
+  dt: number;
+  main: {
+    temp: number;
+  };
+  weather: {
+    description: string;
+  }[];
+}
+
 
 class ChatRobotService {
   private onMessageReceivedCallback: ((response?: Message | Message[]) => void) | null = null;
@@ -13,34 +25,109 @@ class ChatRobotService {
     //('Clearing onMessageReceived callback');
     this.onMessageReceivedCallback = null;
   }
-
-  public addUserMessageAndGenerateResponse(userMessage: string): void {
-    //('Adding user message and generating response');
-    if (userMessage.trim() !== "") {
-      const response = this.generateResponse(userMessage);
-      if (response && this.onMessageReceivedCallback) {
-        //('Sending response to callback:', response);
-        this.onMessageReceivedCallback(response);
-      } else {
-        //('No response generated or no callback set.');
+  // This method fetches weather forecast data from the OpenWeatherMap API
+  private async getWeatherForecast(city: string): Promise<string> {
+    const apiKey = '6bd0bbdc51b704199932e3d3f9fa6f48';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+    
+    console.log('API URL:', apiUrl); // Debugging
+  
+    try {
+      const response = await axios.get(apiUrl);
+      const forecastData: Forecast[] = response.data.list;
+  
+      const weatherTranslations: { [key: string]: string } = {
+        'clear sky': 'selkeä taivas',
+        'few clouds': 'muutamia pilviä',
+        'scattered clouds': 'hajanaisia pilviä',
+        'broken clouds': 'pilvien seassa',
+        'overcast clouds': 'pilvinen',
+        'light rain': 'kevyt sade',
+        'moderate rain': 'kohtalainen sade',
+        'heavy rain': 'rankka sade',
+        'light snow': 'kevyt lumisade',
+        'moderate snow': 'kohtalainen lumisade',
+        'heavy snow': 'runsas lumisade',
+        'thunderstorm': 'ukonilma',
+      };
+  
+      const organizedForecasts: { [key: string]: { time: string; description: string; temperature: number; }[] } = {};
+  
+      forecastData.forEach((forecast) => {
+        const date = new Date(forecast.dt * 1000);
+        const weekday = new Intl.DateTimeFormat('fi-FI', { weekday: 'long' }).format(date);
+        const time = new Intl.DateTimeFormat('fi-FI', { hour: 'numeric', minute: 'numeric' }).format(date);
+  
+        const weatherDescription = forecast.weather[0].description;
+        const translatedWeather = weatherTranslations[weatherDescription] || weatherDescription;
+        const temperature = forecast.main.temp;
+  
+        if (!organizedForecasts[weekday]) {
+          organizedForecasts[weekday] = [];
+        }
+  
+        organizedForecasts[weekday].push({ time, description: translatedWeather, temperature });
+      });
+  
+      // Lasketaan eri viikonpäivien lukumäärä
+      //const uniqueWeekdays = Object.keys(organizedForecasts).length;
+      //console.log('Eri viikonpäivien lukumäärä:', uniqueWeekdays);
+      //console.log('Viikonpäivät:', Object.keys(organizedForecasts));
+  
+      let weeklyForecast = '';
+      for (const day of Object.keys(organizedForecasts)) {
+        weeklyForecast += `<b>${day}</b>:<br />`;
+        const forecasts = organizedForecasts[day].map(forecast => `klo ${forecast.time}, ${forecast.description}, lämpötila: ${forecast.temperature}°C`).join('<br />');
+        weeklyForecast += `${forecasts}<br /><br />`;
       }
-    } else {
-      //('User message is empty.');
+  
+      return `<br /><br /><big><b>6 päivän sääennuste:</b></big><br /><br />${weeklyForecast}`;
+    } catch (error) {
+      console.error('Error in getWeatherForecast:', error);
+      return 'Anteeksi, en pysty tuomaan sääennustetta juuri nyt. Yritä myöhemmin uudelleen.';
     }
   }
+  // Weather forecast data from the OpenWeatherMap API end
 
-  private generateResponse(userMessage: string): Message | undefined {
+
+  public async addUserMessageAndGenerateResponse(userMessage: string): Promise<void> {
+    //('Adding user message and generating response');
+    if (userMessage.trim() !== "") {
+      const response = await this.generateResponse(userMessage);
+      if (response && this.onMessageReceivedCallback) {
+        const resolvedResponse = await Promise.resolve(response) as Message | Message[]; 
+         // If response is a single Message, wrap it in an array
+      const messagesToSend = Array.isArray(resolvedResponse) ? resolvedResponse : [resolvedResponse];
+      //('Sending response to callback:', messagesToSend);
+      this.onMessageReceivedCallback(messagesToSend);
+        
+      }
+    } 
+  }
+
+  private async generateResponse(userMessage: string): Promise<Message | undefined> {
     //('Generating response for user message:', userMessage);
     const trimmedUserMessage = userMessage.trim().toLowerCase();
-    
-    // Tulosta trimmedUserMessage varmistamaan, että se on odotetussa muodossa
-    //('Trimmed user message:', trimmedUserMessage);
 
-    // Tulosta robotAnswers-objektin avaimet, jotta voit tarkistaa, vastaako trimmedUserMessage odotettua avainta
-    //('Robot answers keys:', Object.keys(robotAnswers));
+    // Check if there is a direct response for the trimmed user message
+    if (robotAnswers[trimmedUserMessage]) {
+      const responseMessage = robotAnswers[trimmedUserMessage];
+      const response: Message = {
+        id: Date.now().toString(),
+        senderId: "LAAR Chattirobotti",
+        receiverId: "User",
+        message: responseMessage,
+        text: `LAAR Chattirobotti: ${responseMessage}`,
+        timestamp: new Date(),
+        isUser: false,
+      };
+      return response;
+    }
 
-    if (trimmedUserMessage in robotAnswers) {
-        const responseMessage = robotAnswers[trimmedUserMessage];
+     // Iterate over the keys of robotAnswers and check for keywords
+     for (const keyword of Object.keys(robotAnswers)) {
+      if (trimmedUserMessage.includes(keyword.toLowerCase())) {
+        const responseMessage = robotAnswers[keyword];
         const response: Message = {
             id: Date.now().toString(),
             senderId: "LAAR Chattirobotti",
@@ -52,17 +139,48 @@ class ChatRobotService {
         };
         //('Generated response:', response);
         return response;
-    } else {
-        //('No response generated for user message:', userMessage);
-
-        // Jos ei vastausta, tulosta trimmedUserMessage ja robotAnswers avaimet 
-        // varmistaaksesi, että olet määritellyt vastauksen odotetulle käyttäjäviestille
-
-        //('Trimmed user message:', trimmedUserMessage);
-        //('Robot answers keys:', Object.keys(robotAnswers));
-    }
-    return undefined;
+    } 
   }
-}
 
+  if (/sää|weather/i.test(trimmedUserMessage)) {
+    try {
+      let weatherData;
+
+      if (/tänään|today/i.test(trimmedUserMessage)) {
+        weatherData = await this.getWeatherForecast('Helsinki today');
+      } else {
+        weatherData = await this.getWeatherForecast('Helsinki');
+      }
+
+      const response: Message = {
+        id: Date.now().toString(),
+        senderId: "LAAR Chattirobotti",
+        receiverId: "User",
+        message: weatherData,
+        text: `LAAR Chattirobotti: ${weatherData}`,
+        timestamp: new Date(),
+        isUser: false,
+      };
+      return response;
+    } catch (error) {
+      console.error('Error fetching weather data:', (error as Error).message)
+      return undefined;
+    }
+  }
+
+    // Handle the case when no specific keyword is matched
+    const defaultResponseMessage = 'En ymmärtänyt kysymystäsi. Voisitko kertoa lisää?';
+    const defaultResponse: Message = {
+      id: Date.now().toString(),
+      senderId: "LAAR Chattirobotti",
+      receiverId: "User",
+      message: defaultResponseMessage,
+      text: `LAAR Chattirobotti: ${defaultResponseMessage}`,
+      timestamp: new Date(),
+      isUser: false,
+    };
+    return defaultResponse;
+  }
+
+} 
 export default ChatRobotService;
