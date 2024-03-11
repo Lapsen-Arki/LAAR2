dce360a095ae8f283934cc622440f785b80e366d
Hasse <imberg.hasse@pm.me>
2024-03-11 12:27:29 +0200

    Uudet blockit toimii

--- frontend/src/views/choices.tsx
+++ frontend/src/views/choices.tsx
@@ -98,7 +98,7 @@ export default function ChoicesPage() {
       case "Hyvää yötä":
         setTipsFor("sleep");
         break;
-      case "Nukkuminen":
+      case "Nukkumista":
         setTipsFor("sleep");
         break;
     }
