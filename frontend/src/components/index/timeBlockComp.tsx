57bf525e44540334d75cc8930f1927daf285dc7a
Hasse <imberg.hasse@pm.me>
2024-03-11 12:45:29 +0200

    turha console.log poistettu

--- frontend/src/components/index/timeBlockComp.tsx
+++ frontend/src/components/index/timeBlockComp.tsx
@@ -40,8 +40,6 @@ export default function TimeBlockComp({
           (child) => child.childName === childName
         );
         if (child) {
-          console.log("setting child age: ");
-
           setSelectedChildAge(child.age);
         }
       }
