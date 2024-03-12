3967ff63fb53e9e3c0117e4bfb1d11eec41b2b3d
Hasse <imberg.hasse@pm.me>
2024-03-12 12:10:05 +0200

    small fix

--- frontend/src/components/index/nameDropDown.tsx
+++ frontend/src/components/index/nameDropDown.tsx
@@ -64,7 +64,7 @@ export default function NameDropDown({
   return (
     <>
       {childNames[0]?.length >= 2 && (
-        <Box sx={{ display: "flex-1", justifyContent: "flex-start" }}>
+        <Box sx={{ display: "flex-1", justifyContent: "flex-start", mb: 1 }}>
           <InputLabel id="preview-target-label">Valitse Lapsi</InputLabel>
           <Select
             labelId="preview-target-label"
