16cca27421aac1ec0f0c37732f46c3e56f7dbcc8
jholopai <jholopai@users.noreply.github.com>
2024-03-15 11:15:58 +0200

    Add some error handling

--- backend/src/api/controllers/memos.ts
+++ backend/src/api/controllers/memos.ts
@@ -12,7 +12,8 @@ const saveMemo = async (req: Request, res: Response): Promise<void> => {
     const userDoc = await userDocRef.get();
 
     if (!userDoc) {
-      //ei kayttajaa, heita error
+      res.status(404).json({ error: "Jotain meni vikaan." });
+      return;
     }
 
     const memoDocRef = db.collection("memos").doc(userId);
@@ -30,31 +31,40 @@ const saveMemo = async (req: Request, res: Response): Promise<void> => {
         { merge: true }
       );
     }
-  } catch (error) {}
+  } catch (error) {
+    console.error("Error saving memos: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
 const getMemos = async (req: Request, res: Response): Promise<void> => {
-  const db = admin.firestore();
+  try {
+    const db = admin.firestore();
 
-  const userId = (res as any).userId;
+    const userId = (res as any).userId;
 
-  const userDocRef = db.collection("users").doc(userId);
-  const userDoc = await userDocRef.get();
+    const userDocRef = db.collection("users").doc(userId);
+    const userDoc = await userDocRef.get();
 
-  if (!userDoc) {
-    //ei kayttajaa, heita error
+    if (!userDoc) {
+      res.status(404).json({ error: "Jotain meni vikaan." });
+      return;
+    }
+
+    const memoDocRef = db.collection("memos").doc(userId);
+    const memoDoc = await memoDocRef.get();
+
+    if (!memoDoc.exists) {
+      // Ei ollut muistiinpanoja
+      res.status(200).json([]);
+      return;
+    }
+    const notes: Memo[] = memoDoc.data()?.notes || [];
+    res.status(200).json(notes);
+  } catch (error) {
+    console.error("Error fetching memos: ", error);
+    res.status(500).json({ error: "Internal server error" });
   }
-
-  const memoDocRef = db.collection("memos").doc(userId);
-  const memoDoc = await memoDocRef.get();
-
-  if (!memoDoc.exists) {
-    // Ei ollut muistiinpanoja
-    res.status(200).json([]);
-    return;
-  }
-  const notes: Memo[] = memoDoc.data()?.notes || [];
-  res.status(200).json(notes);
 };
 
 export { saveMemo, getMemos };
