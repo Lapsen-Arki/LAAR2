900dc4f8e1b43eeaa94ff12a515972d646eb8727
LAARlapsen <163561435+LAARlapsen@users.noreply.github.com>
2024-04-28 09:44:51 +0300

    Update blockData.ts

--- frontend/src/components/index/blockData.ts
+++ frontend/src/components/index/blockData.ts
@@ -53,7 +53,7 @@ export const tenavaRytmi = [
   { title: "Aamiainen", period: "7.30-8.30" },
   { title: "Aktiviteetti", period: "8.30-10.45" },
   { title: "Lounas", period: "11.00-12.00" },
-  { title: "Päivälepo", period: "12.00-14.00" },
+  { title: "Päivälepo tai -unet", period: "12.00-14.00" },
   { title: "Välipala", period: "14.00-14.30" },
   { title: "Aktiviteetti", period: "14.30-17.00" },
   { title: "Päivällinen", period: "17.00-17.30" },
